ef7fd64edb7542e3891c3c318cbbf32bbc8b8b6d
ot-babs <toyosi.babs@gmail.com>
2023-09-11 08:25:22 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    # df = pandas.read_csv("")
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+# @app.route("/home")
+# def home():
+#     return render_template("tutorial.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
